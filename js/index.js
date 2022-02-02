5c1431e113ab109e23b2787efdbe4c55be76cce5
BrendoAires <brendoc.aires@gmail.com>
2022-02-02 18:20:34 -0300

    criacao room-homepage

--- js/index.js
+++ js/index.js
@@ -7,14 +7,18 @@ const element = document.getElementById("topMenu");
     if (element.style.height === "70px"){
         document.getElementById("topMenu").style.height = "0"
         document.querySelector('main').classList.remove('filter');
-        document.getElementById("p-grid3").style.color = "hsl(0, 0%, 63%)";
-        document.getElementById("p-grid1" ).style.color = "hsl(0, 0%, 63%)";
+        document.getElementById("p-grid1").style.color = "hsl(0, 0%, 63%)";
+        document.getElementById("p-grid2" ).style.color = "hsl(0, 0%, 63%)";
+        document.getElementById("p-grid3").style.color =  "hsl(0, 0%, 63%)";
+        document.getElementById("p-grid4").style.color =  "hsl(0, 0%, 63%)";
 
     }else{
     document.getElementById("topMenu").style.height = "70px";
     document.querySelector('main').classList.add('filter');
     document.getElementById("p-grid1").style.color = "hsl(0, 0%, 27%)";
+    document.getElementById("p-grid2").style.color = "hsl(0, 0%, 27%)";
     document.getElementById("p-grid3").style.color = "hsl(0, 0%, 27%)";
+    document.getElementById("p-grid4").style.color = "hsl(0, 0%, 27%)";
     //document.querySelector('p').style.color = "red";
     
 
@@ -22,3 +26,35 @@ const element = document.getElementById("topMenu");
    
   }
     })
+
+
+    /*------ Slider ------*/
+
+
+    var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  
+}
