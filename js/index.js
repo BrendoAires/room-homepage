0f5785d86a385ff307f8d04d544b0c029474c8b2
BrendoAires <brendoc.aires@gmail.com>
2022-02-02 15:13:47 -0300

    criacao room-homepage

--- js/index.js
+++ js/index.js
@@ -7,14 +7,15 @@ const element = document.getElementById("topMenu");
     if (element.style.height === "70px"){
         document.getElementById("topMenu").style.height = "0"
         document.querySelector('main').classList.remove('filter');
-        document.getElementById("p-grid1").classList.remove('txt1');
-        document.getElementById("p-grid3").classList.remove('txt1');
+        document.getElementById("p-grid3").style.color = "hsl(0, 0%, 63%)";
+        document.getElementById("p-grid1" ).style.color = "hsl(0, 0%, 63%)";
 
     }else{
     document.getElementById("topMenu").style.height = "70px";
     document.querySelector('main').classList.add('filter');
-    document.getElementById("p-grid1").classList.add('txt1');
-    document.getElementById("p-grid3").classList.add('txt1');
+    document.getElementById("p-grid1").style.color = "hsl(0, 0%, 27%)";
+    document.getElementById("p-grid3").style.color = "hsl(0, 0%, 27%)";
+    //document.querySelector('p').style.color = "red";
     
 
 
